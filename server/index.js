83352da305866e976f1231d34bb397f3946b93b3
TrentDev <TrentLathim@gmail.com>
2022-03-17 16:16:59 -0700

    Created barebones backend and did create react app

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User has disconnected", socket.id);
+  });
+});
+
+server.listen(4000, () => {
+  console.log("server is running ");
+});

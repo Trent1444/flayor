14d17c6609e155930fd0ef19ad1285ab6492c0d9
TrentDev <TrentLathim@gmail.com>
2022-03-17 17:30:34 -0700

    Added more ui and chat component is started

--- server/index.js
+++ server/index.js
@@ -15,10 +15,14 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  console.log(`User Connected: ${socket.id}`);
 
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
   socket.on("disconnect", () => {
-    console.log("User has disconnected", socket.id);
+    console.log(`User Disconnected: ${socket.id}`);
   });
 });
 
